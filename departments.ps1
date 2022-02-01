2ecb9173f8f6699d128596da9ef4838a39ef2989
rschouten97 <69046642+rschouten97@users.noreply.github.com>
2022-02-01 11:36:57 +0100

    Updated to output data record by record

--- departments.ps1
+++ departments.ps1
@@ -1,61 +1,17 @@
 #####################################################
 # HelloID-Conn-Prov-Source-SDBHR
 #
-# Version: 1.0.0.0
+# Version: 2.0.0.0
+# Updated to output data record by record
 #####################################################
 $VerbosePreference = "Continue"
 
-#region Functions functions
-function Get-SDBHRDepartmentData {
-    [CmdletBinding()]
-    param (
-        [Parameter(Mandatory)]
-        [string]
-        $ApiUser,
+$config = $Configuration | ConvertFrom-Json
 
-        [Parameter(Mandatory)]
-        [string]
-        $ApiKey,
-
-        [Parameter(Mandatory)]
-        [string]
-        $KlantNummer,
-
-        [Parameter(Mandatory)]
-        [string]
-        $BaseUrl
-    )
-
-    try {
-        $currentDateTime = (Get-Date).ToString("dd-MM-yyyy HH:mm:ss.fff")
-        $hashedString = New-SDBHRCalculatedHash -ApiKey $ApiKey -KlantNummer $KlantNummer -CurrentDateTime $currentDateTime
-
-        Write-Verbose 'Adding Authorization headers'
-        $headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
-        $headers.Add("Content-Type", "application/json")
-        $headers.Add("Timestamp", $currentDateTime)
-        $headers.Add("Klantnummer", $klantnummer)
-        $headers.Add("Authentication", "$($ApiUser):$($hashedString)")
-        $headers.add("Api-Version", "2.0")
-
-
-        Write-Verbose 'Retrieving departments data'
-        $departmentsList = [System.Collections.generic.List[object]]::new()
-        $splatParams = @{
-            Uri     = "$BaseUrl/api/afdelingen"
-            Headers = $headers
-        }
-        $departmentsResponse = Invoke-SDBHRRestMethod @splatParams
-        $departmentsList.AddRange($departmentsResponse)
-        $departmentsList = $departmentsList | Select-Object *, @{name = 'ExternalId'; expression = { $_.Code } }, @{name = 'DisplayName'; expression = { $_.Omschrijving } }
-
-
-        Write-Output $departmentsList
-    } catch {
-        $PScmdlet.ThrowTerminatingError($_)
-    }
-}
-#endregion Functions
+$ApiUser         = $($config.ApiUser)
+$ApiKey          = $($config.ApiKey)
+$KlantNummer     = $($config.KlantNummer)
+$BaseUrl         = $($config.BaseUrl)
 
 #region Helper Functions
 function New-SDBHRCalculatedHash {
@@ -139,7 +95,7 @@ function Resolve-HTTPError {
         } elseif ($ErrorObject.Exception.GetType().FullName -eq 'System.Net.WebException') {
             $stream = $ErrorObject.Exception.Response.GetResponseStream()
             $stream.Position = 0
-            $streamReader = New-Object System.IO.StreamReader $Stream
+            $streamReader = [System.IO.StreamReader]::new($Stream)
             $errorResponse = $StreamReader.ReadToEnd()
             $HttpErrorObj['ErrorMessage'] = $errorResponse
         }
@@ -148,13 +104,33 @@ function Resolve-HTTPError {
 }
 #endregion Helper Functions
 
-$connectionSettings = $Configuration | ConvertFrom-Json
-$splatParams = @{
-    ApiUser     = $($connectionSettings.ApiUser)
-    ApiKey      = $($connectionSettings.ApiKey)
-    KlantNummer = $($connectionSettings.KlantNummer)
-    BaseUrl     = $($connectionSettings.BaseUrl)
-}
-$departments = Get-SDBHRDepartmentData @splatParams
-Write-Output $departments | ConvertTo-Json -Depth 10
+try{
+    $currentDateTime = (Get-Date).ToString("dd-MM-yyyy HH:mm:ss.fff")
+    $hashedString = New-SDBHRCalculatedHash -ApiKey $ApiKey -KlantNummer $KlantNummer -CurrentDateTime $currentDateTime
 
+    Write-Verbose 'Adding Authorization headers'
+    $headers = [System.Collections.Generic.Dictionary[[String],[String]]]::new()
+    $headers.Add("Content-Type", "application/json")
+    $headers.Add("Timestamp", $currentDateTime)
+    $headers.Add("Klantnummer", $klantnummer)
+    $headers.Add("Authentication", "$($ApiUser):$($hashedString)")
+    $headers.add("Api-Version", "2.0")
+
+
+    Write-Verbose 'Retrieving departments data'
+    $departmentsList = [System.Collections.generic.List[object]]::new()
+    $splatParams = @{
+        Uri     = "$BaseUrl/api/afdelingen"
+        Headers = $headers
+    }
+    $departmentsResponse = Invoke-SDBHRRestMethod @splatParams
+    $departmentsList.AddRange($departmentsResponse)
+    Write-Verbose "Found $($departmentsResponse.Count) departments. Creating list of departments to return"
+
+    $departmentsList = $departmentsList | Select-Object *, @{name = 'ExternalId'; expression = { $_.Code } }, @{name = 'DisplayName'; expression = { $_.Omschrijving } }
+    foreach ($department in $departmentsList) {
+        Write-Output $department | ConvertTo-Json -Depth 10
+    }
+} catch {
+    throw $_
+}
